251cb9025c40b703553368aebca3170277341f84
Ryan Thornhill <ryant26@gmail.com>
2013-04-05 12:49:50 -0600

    Frams sort of working

--- Interface.py
+++ Interface.py
@@ -9,13 +9,15 @@ root = Tk()
 #Global Variables
 global canvas_width
 global canvas_height
-global canvas_column
-global canvas_row
+global frame_column
+global frame_row
 global number_of_days
 global hours_in_day
 
 canvas_width = 600
 canvas_height = 400
+frame_column = 1
+frame_row = 1
 number_of_days = 7
 hours_in_day = 10
 
@@ -26,39 +28,24 @@ create_new = ttk.Button(root, text='New Appointment', padding=(5,5,5,5))
 schedule_frame = ttk.Frame(root, relief = "sunken", borderwidth = 5, padding=(3,3,3,3))
 
 # Set up our Canvas
-canvas = Canvas(schedule_frame, width = canvas_width, height=canvas_height)
+#canvas = Canvas(schedule_frame, width = canvas_width, height=canvas_height)
 
 #A loop to create a grid of frames
 days_of_week = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 for i in range(number_of_days):
-	for j in range(hours_in_day):
-		ttk.Frame(schedule_frame, relief='flat',labels= days_of_week[i], str(8+j)).grid(sticky=)
 
-#Create all the labels we will need
-sunday = ttk.Label(text='Sunday')
-monday = ttk.Label(text='Monday')
-tuesday = ttk.Label(text='Tuesday')
-wednesday = ttk.Label(text='Wednesday')
-thursday = ttk.Label(text="Thursday")
-friday = ttk.Label(text='Friday')
-saturday = ttk.Label(text = 'Saturday')
+	ttk.Label(text=str(days_of_week[i])).grid(row = 0, column = i+1)			#create the day Labels
+
+	for j in range(hours_in_day+1):
+		ttk.Frame(root, relief='solid', width = 70, height = 50, borderwidth = 1).grid(sticky='nwes', column = i+1, row = j+1, columnspan = 1, rowspan = 1)
 
 #------------------------------------------grid all widgets------------------------------------------------
 #Frame
-frame.grid(column = 1, row = 1, columnspan = 7, rowspan= 10) 
+schedule_frame.grid(column = frame_column, row = frame_row, columnspan = 7, rowspan= 10) 
 
 #canvas
-canvas.grid()
+#canvas.grid()
 #grid
 create_new.grid(column= 0, row=0)
-
-#days of the week
-sunday.grid(column=1, row=0)
-monday.grid(column=2, row=0)
-tuesday.grid(column=3, row=0)
-wednesday.grid(column=4, row=0)
-thursday.grid(column=5,row=0)
-friday.grid(column=6, row=0)
-saturday.grid(column=7, row=0)
 
 root.mainloop()	
